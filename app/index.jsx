654bcde8a3dc0aa9dd8f2c282c20e9e5edb15995
Chris Keathley <c@keathley.io>
2015-07-03 23:28:37 -0400

    Get slides working

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,77 @@
-var React = require('react')
-  , Router = require('react-router');
-
-var App = require('./components/App')
-  , Hello = require('./components/Hello')
+import React from 'react'
+import { Router, Route, Navigation } from 'react-router'
+import { history } from 'react-router/lib/BrowserHistory'
 
 require('./styles/index.scss');
 
-var routes = (
-  <Router.Route name="app" path="/" handler={App}>
-    <Router.DefaultRoute handler={Hello} />
-  </Router.Route>
-);
+const LEFT  = 37
+const RIGHT = 39
 
-Router.run(routes, Router.HistoryLocation, (Handler, state) => {
-  React.render(<Handler/>, document.body);
-});
+const App = React.createClass({
+  mixins: [ Navigation ],
+
+  componentWillMount(){
+    document.addEventListener("keydown", this.handleKeyDown, false);
+  },
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown, false);
+  },
+
+  handleKeyDown(event) {
+    if ( event.keyCode != LEFT && event.keyCode != RIGHT ) return;
+
+    var key   = event.keyCode
+      , id    = this.props.params.id ? parseInt(this.props.params.id) : 0
+      , newId = key == LEFT ? Math.max(0, id - 1) : (id + 1)
+      , newId = newId === 0 ? '' : newId
+
+    this.transitionTo('/' + newId)
+  },
+
+  render () {
+    return (
+      <main className='app'>
+        <Slide id={ null } { ...this.props } >
+          <h1>First Slide</h1>
+        </Slide>
+
+        <Slide id={ 1 } { ...this.props } >
+          <h1>Second Slide</h1>
+        </Slide>
+
+        <Slide id={ 2 } { ...this.props }>
+          <h1>Third Slide</h1>
+        </Slide>
+      </main>
+    )
+  }
+})
+
+const Slide = React.createClass({
+  render() {
+    var visible = this.props.id == this.props.params.id
+      , styles = { display: (visible ? 'block' : 'none') }
+
+    return (
+      <section className="slide" style={ styles }>
+        <h1>this is my slide</h1>
+        { this.props.children }
+      </section>
+    )
+  }
+})
+
+var SlideRoute = {
+  name: "slide",
+  path: "/:id",
+  component: Slide
+}
+
+var routes = {
+  path: "/",
+  component: App,
+  childRoutes: [ SlideRoute ]
+}
+
+React.render(<Router history={ history } children={ routes } />, document.body)
